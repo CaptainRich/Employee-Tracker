4f532c46b1eedeffd4fd84cec8c8fcb919d744c6
RichA <rich_ay@sbcglobal.net>
2020-10-17 17:39:12 -0500

    Clean up complete

--- index.js
+++ index.js
@@ -4,9 +4,6 @@
 const inquirer    = require( 'inquirer' );
 const mysql       = require( 'mysql2' );
 
-// Import the other modules
-const { getRoles, addRole } = require('./lib/roles');
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // Setup the connection to the database, and if successful invoke the Main Menu.
@@ -54,7 +51,7 @@ function mainMenu()  {
                     break;
 
                 case "View All Roles":
-                    getRoles();
+                    getRoles( connection );
                     break;
                     
                 case "View All Employees":
@@ -66,7 +63,7 @@ function mainMenu()  {
                     break;
                                                             
                 case "Add a Role":
-                    addRole();
+                    addRole( connection);
                     break;
                                                                                 
                 case "Add an Employee":
@@ -96,6 +93,23 @@ function getDepartments() {
 
     connection.query(
         'SELECT * FROM department', ( err, res ) => {
+            if( err ) throw err;                               // abort on a failure
+            console.table( res );
+            mainMenu();
+        }
+    
+    );
+};
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Routine to obtain all of the company's employee roles.
+function getRoles( connection ) {
+
+    console.log('Current employee roles');
+
+    connection.query(
+        'SELECT * FROM role', ( err, res ) => {
             if( err ) throw err;                               // abort on a failure
             console.table( res );
             mainMenu();
@@ -172,6 +186,73 @@ function createDepartment( department_name ) {
 }
 
 
+
+//////////////////////////////////////////////////////////////////////////////
+// Routine to add a new employee role.
+function addRole( connection ) {
+
+    console.log('Adding a new role');
+    inquirer.prompt( [
+        {
+            name: "title",
+            message: "What is the new role name?",
+            type: "text",
+            validate: titleNameInput => {
+                if (titleNameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the role name!");
+                    return false;
+                }
+            }
+        },
+        {
+            name: "salary",
+            message: "What is the salary for this role?",
+            type: "text" ,
+            validate: salaryInput => {
+                if (salaryInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the salary for this role!");
+                    return false;
+                }
+            }
+        },
+        {
+            name: "department_id",
+            message: "What is the department role?",
+            type: "text" ,
+            validate: department_idInput => {
+                if (department_idInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the department this role!");
+                    return false;
+                }
+            }
+        }
+    ])
+    .then( data => {
+        createRole (data, connection)
+        .then( console.log("Role added") )
+        .then( ()=> mainMenu() );
+    })
+};
+
+//////////////////////////////////////////////////////////////////////
+// Promise function to perform the addition to the database
+function createRole(data, connection) {  
+
+    //console.log(data);
+
+    return connection.promise().query( 'INSERT INTO role SET ?', 
+                                       {title: data.title, salary: data.salary, department_id: data.department_id},
+        (err, res) => {
+            if (err) throw err;                      // abort on a failure
+            console.table(res);
+        });
+};
 
 //////////////////////////////////////////////////////////////////////////////
 // Routine to add a new employee.
